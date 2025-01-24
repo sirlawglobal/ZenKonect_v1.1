fa22ca1f4eb3ea7af4c117d84446d45ca438c464
Danielkuku <cristobaldaniel022@gmail.com>
2025-01-24 17:06:46 +0000

    API-integration

--- zenkonect/src/components/Views/App.jsx
+++ zenkonect/src/components/Views/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Header from "../Helper/Header";
+
 import LoginApp from "../Views/LoginApp";
 import Home from "../Helper/Home";
 import CreateAccount from "../Views/CreateAccount";
@@ -7,7 +7,6 @@ import CreateAccount from "../Views/CreateAccount";
 function App() {
   return (
     <Router>
-      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<LoginApp />} />
