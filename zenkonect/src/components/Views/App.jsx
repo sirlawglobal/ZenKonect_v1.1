e57d1a42230b9bebecc6c6c14fb39a74bed7f3ee
Danielkuku <cristobaldaniel022@gmail.com>
2025-01-29 14:08:37 +0000

    photo upload

--- zenkonect/src/components/Views/App.jsx
+++ zenkonect/src/components/Views/App.jsx
@@ -1,7 +1,6 @@
-
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-import LoginApp from "./components/";
+import LoginApp from "../Views/LoginApp";
 import Home from "../Helper/Home";
 import CreateAccount from "../Views/CreateAccount";
 import AddPicture from "./AddPicture";
