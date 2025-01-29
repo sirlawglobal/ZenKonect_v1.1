66ea6fd363e7978a257a209291adae6ad372754f
Roni <ronniiiip@gmail.com>
2025-01-29 08:49:00 +0000

    bug fixes

--- zenkonect/src/components/Views/App.jsx
+++ zenkonect/src/components/Views/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "../Helper/Header"
 import LoginApp from "../Views/LoginApp";
+import Dashboard from "../Views/Dashboard";
 import Home from "../Helper/Home";
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<LoginApp />} />
+        <Route path="/dashboard" element={<Dashboard />} />
       </Routes>
     </Router>
   );
